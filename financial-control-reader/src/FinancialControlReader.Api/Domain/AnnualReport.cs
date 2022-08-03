08672ff64e2d925ef78b6f87e169163fe65a83ed
Samuel Couto <samuel.couto92@gmail.com>
2022-08-03 20:22:24 +0200

    feat: add AnnualReport class

---
+++ financial-control-reader/src/FinancialControlReader.Api/Domain/AnnualReport.cs
@@ -0,0 +1,24 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FinancialControlReader.Api.Domain;
+public class AnnualReport
+{
+  public AnnualReport(string id) 
+  {
+    Id = id;
+  }
+
+	[BsonId]
+  public string Id { get; set; } 
+
+  public ICollection<AnnualReportData>? Data { get; set; }
+}
+
+public class AnnualReportData
+{
+  public int Month { get; set; } 
+
+	[BsonRepresentation(BsonType.Double)]
+ 	public double Total { get; set; }
+}
